18f280462f41833a4ca7e96592df9b9f417de5b6
LAPTOP-RO7RMO41\satos <satosato3104an@gmail.com>
2023-05-02 08:26:20 +0900

    20230502実験

---
+++ cmake-build-debug/CMakeFiles/CompetitiveProgramming.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompetitiveProgramming.dir/main.cpp.obj"
+  "CMakeFiles/CompetitiveProgramming.dir/main.cpp.obj.d"
+  "CompetitiveProgramming.exe"
+  "CompetitiveProgramming.exe.manifest"
+  "CompetitiveProgramming.pdb"
+  "libCompetitiveProgramming.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CompetitiveProgramming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
