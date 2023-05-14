032f9feee2c2eb0634ee2a48b0b40ff080e22286
LAPTOP-RO7RMO41\satos <satosato3104an@gmail.com>
2023-05-14 23:14:04 +0900

    202305014

--- cmake-build-debug/CMakeFiles/CompetitiveProgramming.dir/main.cpp.obj.d
+++ cmake-build-debug/CMakeFiles/CompetitiveProgramming.dir/main.cpp.obj.d
@@ -135,4 +135,9 @@ CMakeFiles/CompetitiveProgramming.dir/main.cpp.obj: \
  C:/PROGRA~2/MINGW-~1/I686-8~1.0-P/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/c++/bits/stl_tempbuf.h \
  C:/PROGRA~2/MINGW-~1/I686-8~1.0-P/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/c++/bits/stl_construct.h \
  C:/PROGRA~2/MINGW-~1/I686-8~1.0-P/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/c++/bits/uniform_int_dist.h \
- C:/PROGRA~2/MINGW-~1/I686-8~1.0-P/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/c++/limits
+ C:/PROGRA~2/MINGW-~1/I686-8~1.0-P/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/c++/limits \
+ C:/PROGRA~2/MINGW-~1/I686-8~1.0-P/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/c++/vector \
+ C:/PROGRA~2/MINGW-~1/I686-8~1.0-P/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/c++/bits/stl_uninitialized.h \
+ C:/PROGRA~2/MINGW-~1/I686-8~1.0-P/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/c++/bits/stl_vector.h \
+ C:/PROGRA~2/MINGW-~1/I686-8~1.0-P/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/c++/bits/stl_bvector.h \
+ C:/PROGRA~2/MINGW-~1/I686-8~1.0-P/mingw32/lib/gcc/i686-w64-mingw32/8.1.0/include/c++/bits/vector.tcc
--- main.cpp
+++ main.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <algorithm>
-constexpr int INF = 1000000000;
-int N, Q, A[300010];
+
+using namespace std;
+
 int main() {
-    std::cin >> N;
-    for (int i = 0; i < N; i++) std::cin >> A[i];
-    std::sort(A, A + N);
-    std::cin >> Q;
-    for (int i = 0; i < Q; i++) {
-        int B;
-        std::cin >> B;
-        int ans = INF;
-        auto p = std::upper_bound(A, A + N, B);
-        if (p != A + N) ans = std::min(ans, *p - B);
-        if (p != A) ans = std::min(ans, B - *std::prev(p));
-        std::cout << ans << std::endl;
+    int N;
+    string S;
+    cin >> N;
+    cin >> S;
+    for(int i=0;i<N;i++){
+        if(S[i]=='T'){
+
+        }
     }
     return 0;
 }
