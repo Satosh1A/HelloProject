5fab9a0b78de53b593c6060f409319477d204ea8
LAPTOP-RO7RMO41\satos <satosato3104an@gmail.com>
2023-05-25 23:07:47 +0900

    202305025

--- main.cpp
+++ main.cpp
@@ -8,9 +8,26 @@ typedef long long ll;
 using namespace std;
 
 int main() {
-    int H, W;
-    cin >> H >> W;
-    if(H==1||W==1) cout << H*W << endl;
-    else cout << (H+1)/2*((W+1)/2) << endl;
+    int N, P, Q, ans=0;
+    vector<int> A(N);
+    cin >> N >> P >> Q;
+    for(int i=0;i<N;i++){
+        cin >> A[i];
+        A[i] %= P;
+    }
+    for(int i=0;i<N-4;i++){
+        for(int j=i+1;j<N-3;j++){
+            for(int k=j+1;k<N-2;k++){
+                for(int l=k+1;l<N-1;l++){
+                    for(int m=l+1;m<N;m++){
+                        if(((((A[i]*A[j])%P*A[k])%P*A[l])%P*A[m])%P==Q){
+                            ans++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
